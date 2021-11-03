ff6ad1aaae1e3c9b73eff3ba5d7537920023db2c
“leonshimizu” <lmshimizu@gmail.com>
2021-11-02 19:34:02 -0700

    js practice

---
+++ loops.js
@@ -0,0 +1,18 @@
+// var fruits = ['apple', 'banana', 'orange', 'kiwi'];
+
+// while loop
+// var i = 0;
+// while (i < fruits.length) {
+//   console.log(fruits[i]);
+//   i += 1
+// }
+
+// forEach loop
+// fruits.forEach(function(fruit) {
+//   console.log(fruit);
+// });
+
+// for loop
+/*for (var i = 0; i < fruits.length; i ++) {
+  console.log(fruits[i]);
+}*/
